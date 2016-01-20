6a082244318853207cff15125cd2d77269a67cc7
patrocc6 <patrocc6@gmail.com>
2016-01-20 12:41:29 +0000

    Done.

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,23 @@
 # Implement your procedural solution here!
+def even_fibonacci_sum(limit)
+  even_fibonacci = fibonacci(limit).select{|num| num.even?}
+  return even_fibonacci.inject(:+) 
+end
+
+def fibonacci(limit)
+  fibonacci_numbers = [1,2,3]
+
+  idx = 3
+  while true
+    fibonacci_numbers[idx] = fibonacci_numbers[idx-1] + fibonacci_numbers[idx-2]
+
+    if fibonacci_numbers[idx] >= limit
+      fibonacci_numbers.pop
+      break
+    end
+
+    idx += 1
+  end
+
+  return fibonacci_numbers
+end
--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -1,1 +1,29 @@
 # Implement your object-oriented solution here!
+class EvenFibonacci
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def sum
+    even_fibonacci = self.fibonacci.select{|num| num.even?}
+    return even_fibonacci.inject(:+)
+  end
+
+  def fibonacci
+    fibonacci_numbers = [1,2,3]
+
+    idx = 3
+    while true
+      fibonacci_numbers[idx] = fibonacci_numbers[idx-1] + fibonacci_numbers[idx-2]
+
+      if fibonacci_numbers[idx] >= @limit
+        fibonacci_numbers.pop
+        break
+      end
+
+      idx += 1
+    end
+
+    return fibonacci_numbers
+  end
+end
